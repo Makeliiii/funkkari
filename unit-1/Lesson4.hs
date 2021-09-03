15a69206dad74a895a7321c07717632c213f982a
Mäkeli <joel.m.maenpaa@gmail.com>
2021-09-03 14:12:12 +0300

    ye asd fuck closures

--- unit-1/Lesson4.hs
+++ unit-1/Lesson4.hs
@@ -80,3 +80,64 @@ compareLastNamesNew name1 name2 = if comparisonResult == EQ
 waOffice :: ([Char], [Char]) -> [Char]
 waOffice name = nameText ++ " Esq - PO Box 666 - Washington, DC 66666"
     where nameText = fst name ++ " " ++ snd name
+
+-- listing 5.1
+inc :: Num a => a -> a
+inc n = n + 1
+
+double :: Num a => a -> a
+double n = n * 2
+
+square :: Num a => a -> a
+square n = n^2
+
+ifEven :: Integral p => (p -> p) -> p -> p
+ifEven func n = if even n
+           then func n
+           else n
+
+genIfEven :: Integral p => (p -> p) -> p -> p
+genIfEven f = (\x -> ifEven f x)
+
+ifEvenInc :: Integer -> Integer
+ifEvenInc = genIfEven inc
+
+genIfXEven :: Integral p => p -> (p -> p) -> p
+genIfXEven x = (\f -> ifEven f x)
+
+-- listing 5.2
+getRequestUrl :: [Char] -> [Char] -> [Char] -> [Char] -> [Char]
+getRequestUrl host apiKey resource id = host ++
+                                        "/" ++
+                                        resource ++
+                                        "/" ++
+                                        id ++
+                                        "?token=" ++
+                                        apiKey
+                    
+genHostRequestBuilder :: [Char] -> [Char] -> [Char] -> [Char] -> [Char]
+genHostRequestBuilder host = (\apiKey resource id ->
+                               getRequestUrl host apiKey resource id)
+
+-- listing 5.3
+exampleUrlBuilder :: [Char] -> [Char] -> [Char] -> [Char]
+exampleUrlBuilder = genHostRequestBuilder "https://example.com"
+
+-- listing 5.4
+genApiRequestBuilder :: (t1 -> t2 -> t3 -> t4) -> t1 -> t2 -> t3 -> t4
+genApiRequestBuilder hostBuilder apiKey = (\resource id ->
+                                            hostBuilder apiKey resource id)
+
+-- listing 5.5
+myExampleUrlBuilder :: [Char] -> [Char] -> [Char]
+myExampleUrlBuilder = genApiRequestBuilder exampleUrlBuilder "1337hAsk3ll"
+
+-- quick check 5.2
+genApiRequestBuilderWithResource :: (t1 -> t2 -> t3 -> t4) -> t1 -> t2 -> t3 -> t4
+genApiRequestBuilderWithResource hostBuilder apiKey resource = (\id ->
+                                                                 hostBuilder apiKey resource id)
+
+myExampleUrlBuilderTwo :: [Char] -> [Char]
+myExampleUrlBuilderTwo = genApiRequestBuilderWithResource exampleUrlBuilder "1337hAsk3ll" "book" 
+
+-- listing 5.6
