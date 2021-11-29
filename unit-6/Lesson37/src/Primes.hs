fd2f10930c0db33f2a4879bc39ce34de5062304f
Mäkeli <joel.m.maenpaa@gmail.com>
2021-11-29 16:56:18 +0200

    yup

--- unit-6/Lesson37/src/Primes.hs
+++ unit-6/Lesson37/src/Primes.hs
@@ -1,6 +1,31 @@
 module Primes where
 
 primes :: [Int]
-primes = [1 ..]
+primes = sieve [2 .. 10000]
 
+sieve :: [Int] -> [Int]
+sieve [] = []
+sieve (x:xs) = x : sieve noFactors
+    where
+        noFactors = filter ((/= 0) . (`mod` x)) xs
 
+isPrime :: Int -> Maybe Bool 
+isPrime n 
+    | n < 2 = Nothing 
+    | n >= length primes = Nothing
+    | otherwise = Just (n `elem` primes)
+
+unsafePrimeFactors :: Int -> [Int] -> [Int]
+unsafePrimeFactors 0 [] = []
+unsafePrimeFactors n [] = []
+unsafePrimeFactors n (x:xs) = if n `mod` x == 0
+                              then x : unsafePrimeFactors (n `div` x) (x:primes)
+                              else unsafePrimeFactors n primes
+
+primeFactors :: Int -> Maybe [Int]
+primeFactors n
+    | n < 2 = Nothing
+    | n >= length primes = Nothing 
+    | otherwise = Just (unsafePrimeFactors n primesLessThanN)
+        where
+            primesLessThanN = filter (<= n) primes
