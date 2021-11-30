566650a50dc1151b252ac181715f1d93246b8c92
Mäkeli <joel.m.maenpaa@gmail.com>
2021-11-30 16:05:42 +0200

    rest

--- unit-7/Lesson39/http-lesson/app/Main.hs
+++ unit-7/Lesson39/http-lesson/app/Main.hs
@@ -1,6 +1,33 @@
 module Main where
 
-import Lib
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as LC
+import Network.HTTP.Simple (Request, setRequestMethod, setRequestHost, setRequestHeader, setRequestPath, setRequestSecure, setRequestPort, defaultRequest, httpLBS, getResponseStatusCode, getResponseBody)
+
+myToken :: BC.ByteString
+myToken = "token"
+
+noaaHost :: BC.ByteString
+noaaHost = "www.ncdc.noaa.gov"
+
+apiPath :: BC.ByteString
+apiPath = "/cdo-web/api/v2/datasets"
+
+buildRequest :: BC.ByteString -> BC.ByteString -> BC.ByteString -> BC.ByteString -> Request
+buildRequest token host method path = setRequestMethod method $ setRequestHost host $ setRequestHeader "token" [token] $ setRequestPath path $ setRequestSecure True $ setRequestPort 443 $ defaultRequest 
+
+request :: Request 
+request = buildRequest myToken noaaHost "GET" apiPath
 
 main :: IO ()
-main = someFunc
+main = do
+    response <- httpLBS request
+    let status = getResponseStatusCode response
+    if status == 200
+        then do
+            print "saving request to a file"
+            let jsonBody = getResponseBody response
+            L.writeFile "data.json" jsonBody
+        else print "request failed with error"
